1b755f9ba07fca728e9abf2a5c9b541caf5b2f22
srija81250 <srija9@gmail.com>
2026-07-28 13:40:44 +0530

    project completed

---
+++ student_management/student_management_pro/student_management_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Student,Marks
+# Register your models here.
+
+
+
+admin.site.register(Student)
+admin.site.register(Marks)
---
+++ student_management/student_management_pro/student_management_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('add_student/',views.add_student),
+    path('add_marks/',views.add_marks),
+    path('view_student/',views.view_student),
+    path('all_students/', views.all_students),
+
+]
