454acf2ca1fd620b2b3a5eaa9e85a272ffa9137d
srija81250 <thsrija9@gmail.com>
2026-07-28 13:13:47 +0530

    day 3 task completed

---
+++ day3_task.py
@@ -0,0 +1,128 @@
+
+#print statement:
+print("My name is Srija")
+#data stuctures and data types:
+#define a list containing three different data types:
+sample_list=[5,8.95,"hello world"]  #list is an ordered sequence of elements
+print(sample_list)
+print(type(sample_list))
+#define a set containing employee id's:
+employee_id={100,101,102,103,104,105}
+print(employee_id)
+print(type(employee_id))
+#string operations:
+#concatenate two strings and print the result:
+first_name="SRIJA"
+last_name="THALLA"
+full_name=first_name+" "+last_name
+print(full_name)
+print(type(full_name))
+#repeat a string 3 times and display output:
+emp_name="srija"
+sample=(emp_name+" ")*3
+print(sample)
+
+
+
+
+
+"""
+
+#python variables:
+#declare 2 variables,one storing an integer and other storing a string.print their values
+emp_name="SRIJA"
+emp_id=123
+print(emp_name)
+print(emp_id)
+
+#type conversions:
+#convert a float to an integer and print the result.
+emp_salary=85000.00
+print(type(emp_salary))
+sample=int(emp_salary)
+print(type(sample))
+#convert an integer to a string and display the output
+age=30
+sample_age=str(age)
+print(type(sample_age))
+
+#simple input and output:
+#take the user's age as input and print a message using that input
+sample_age_1=int(input("Enter your age:"))
+print("Your age is",+sample_age_1)
+
+
+#python keywords:
+#create a variable with a name that is python keyword.what happens?
+continue="python"
+
+
+
+
+#simple input and output:
+#build a calculator program that takes two numbers as an input and performs arthmetic operations
+maths_marks=98
+physics_marks=99
+total_marks=maths_marks+physics_marks
+print(total_marks)
+
+#type conversions:
+'''create a program that takes user input for their age,converts it to an integer,
+adds 5 and print the result '''
+age_1=input("Enter your age: ")
+sample_age=int(age_1)
+sample_age_2=sample_age+5
+print(sample_age_2)
+
+#concatenate strings:
+#write a program to take two names as input and print them together
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name:")
+full_name=first_name+" "+last_name
+print(full_name)"""
+
+
+
+
+
+'''
+#data structures and data types:
+#implement a program that uses a dictionary to store information about a book
+book_dictionary={
+    "title":"five point someone",
+    "author":"chetan bhagat",
+    "publication_year":2004
+}
+print(book_dictionary)
+print(type(book_dictionary))
+
+#print statement:
+#write a program that prints a pattern using multiple print statements
+print("*")
+print("* *")
+print("* * *")
+print("* * * *")
+print("* * * * *")'''
+
+
+
+#Create a Python script for a simple task and add comments to explain each step.
+'''
+first line indicates marks secured in subject mathematics
+second line indicates marks secured in subject physics
+third line indicates marks secured in subject chemistry
+in fourth line by using + operator addition of all the 3 subjects has done to get
+ the total marks
+ last line is written to print total marks on the screen
+ '''
+"""mathematics=98
+physics=99
+chemistry=99
+total_marks=mathematics+physics+chemistry
+print(total_marks)
+
+#string operations:
+#write a python program that takes string as input(35) and return floar value
+num_1=input("enter a value: ")
+num_2=float(num_1)
+print(type(num_2))"""
